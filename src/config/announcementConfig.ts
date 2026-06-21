9ca84e1b2df28381228a11664e45b9c658ab0ca3
hcb1729 <861999067@qq.com>
2026-06-21 21:11:23 +0800

    config更新

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,7 +5,7 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	content: "欢迎来到我的博客！我是一位普通的技术爱好者，可以叫我所追寻的风。",
 
 	// 是否允许用户关闭公告
 	closable: true,

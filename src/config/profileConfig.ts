0856d3b0a6c63fa095b8184ec4d2ad181751a102
hcb1729 <861999067@qq.com>
2026-06-22 17:27:09 +0800

    config更新

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/头像.png",
 
 	// 名字
-	name: "Firefly",
+	name: "所追寻的风",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "一位普通的技术爱好者，爱好ACG，户外，羽毛球，足球，马拉松。",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
